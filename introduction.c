c9c21c50c9a4dc6ee95669e58b91a2439b696da3
PeterBYUI <pierre.gavino@gmail.com>
2026-01-22 17:26:44 +0100

    variables

--- introduction.c
+++ introduction.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
-  return 0;
+    printf("\tDear world,\n");
+    printf("\tMy name is Pierre");
+
+    char letter = 'p';
+    printf("\n\tLet it be known that my favorite letter is %c", letter);
+
+    char name[] = "Pierre";
+    int age = 27;
+    printf("\n\tMy name is %s and I am %d years old.", name, age);
+    printf("\n\tI live in %s.", "Dijon");
+
+    int x = -5;
+    int y = 6;
+    int sum = x + y;
+    printf("\n\tResult: %d", sum);
+    //Pick up at "Multiple variables"
+    return 0;
 }
